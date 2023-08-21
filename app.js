4efa7939ba6d48eede0293cc56dc054dec5296af
MrMolina836 <robertomolina8.361110@gmail.com>
2023-08-21 14:20:53 -0400

    Carousel complete for About Page

--- app.js
+++ app.js
@@ -19,3 +19,36 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false);
     }
 })
+
+const carousel = document.querySelector('.carousel');
+const arrowBtns = document.querySelectorAll('.wrapper i');
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+
+let isDragging = false, startX, startScrollLeft;
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft = btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+})
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mousemove", dragStart);
+document.addEventListener("mouseup", dragStop);
